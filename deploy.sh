3718722a82ed9c2dd8c475d98c7001f27598a379
eric <toericliu@qq.com>
2018-11-02 18:34:18 +0800

    update

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-USER=ubuntu
-HOST=www.apower.top
-DIR=documents/blog   # might sometimes be empty!
-
-hugo && rsync -avz --delete public/ ${USER}@${HOST}:~/${DIR}
-
-exit 0
---
+++ scp-blog.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp -r /mnt/c/space/web/blog/public ubuntu@www.usepower.net:/home/ubuntu
